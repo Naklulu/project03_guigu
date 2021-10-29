105e625b188550919f109d845083a536117e8939
fengjunhua <2783674649@qq.com>
2021-10-29 12:34:31 +0800

    first_commit

---
+++ src/com/atguigu/team/com/atguigu/team/junit/NameListServiceTest.java
@@ -0,0 +1,21 @@
+package com.atguigu.team.com.atguigu.team.junit;
+
+import com.atguigu.team.domain.Employee;
+import com.atguigu.team.service.NameListService;
+import org.junit.Test;
+
+import java.sql.SQLOutput;
+
+public class NameListServiceTest {
+    @Test
+    public void testgetAllEmployees(){
+        NameListService service = new NameListService();
+        Employee[] employees = service.getAllEmployees();
+        for (int i = 0;i < employees.length;i++){
+            System.out.println(employees[i]);
+
+        }
+    }
+
+
+}
